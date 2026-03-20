3d088f00b1449ed702ac761e6fad8f4602025f81
AdrielSanUEA77 <ae.sanchezp@uea.edu.ec>
2026-03-20 12:06:42 -0500

    Tarea_poo_visitas
    Este programa es una aplicación de escritorio hecha en Python con Tkinter que permite gestionar visitantes mediante un sistema CRUD organizado en una arquitectura modular por capas: la capa Modelo define la clase Visitante con sus datos (cédula, nombre y motivo), la capa Servicio administra internamente la lista de visitantes y ofrece métodos para agregar, eliminar y obtener registros, y la capa UI contiene la interfaz gráfica con campos de entrada, botones y una tabla Treeview; desde esta interfaz, el usuario puede registrar nuevos visitantes (validando que los campos no estén vacíos), visualizar automáticamente la lista actualizada en la tabla, seleccionar un visitante para eliminarlo y limpiar los campos del formulario, mientras que el archivo main.py solo inicia el servicio, carga la interfaz y ejecuta la aplicación, garantizando así una separación clara entre lógica, datos y presentación.

---
+++ tarea_poo_visitas/main.py
@@ -0,0 +1,8 @@
+
+from servicios.visita_servicio import VisitaServicio
+from ui.app_tkinter import AppTkinter
+
+if __name__ == "__main__":
+    servicio = VisitaServicio()
+    app = AppTkinter(servicio)
+    app.mainloop()
---
+++ tarea_poo_visitas/modelos/visitante.py
@@ -0,0 +1,6 @@
+
+class Visitante:
+    def __init__(self, cedula, nombre, motivo):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.motivo = motivo
